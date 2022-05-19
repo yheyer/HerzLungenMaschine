07fd913ba18c853955945ef250801f96066cf436
Rafael Gufler <rafael@calist.io>
2022-05-19 10:39:47 +0200

    Utilities strukturiert und Kommentare hinzugefügt

--- ProjectFiles/utilities.py
+++ ProjectFiles/utilities.py
@@ -1,18 +1,20 @@
 # Import external packages
-
 from multiprocessing.connection import wait
 import pandas as pd
 from datetime import datetime
 import numpy as np
 import re
 
-# Classes 
 
+# Klasse Subject
 class Subject():
+    
+    # Konstruktor mit file_name als Parameter (aus welchen Daten für Subject gelesen werden)
     def __init__(self, file_name):
 
         ### Aufgabe 1: Interpolation ###
 
+        # Lesen der Daten aus csv Datei
         __f = open(file_name)
         self.subject_data = pd.read_csv(__f)
 
@@ -20,6 +22,7 @@ class Subject():
         # ist hier linear oder nearest am sinnvollsten.
         self.subject_data = self.subject_data.interpolate(method="nearest", axis=0) 
         
+        # Variablen der Klasse anlegen
         __splited_id = re.findall(r'\d+',file_name)      
         self.subject_id = ''.join(__splited_id)
         self.names = self.subject_data.columns.values.tolist()
@@ -28,9 +31,6 @@ class Subject():
         self.temp = self.subject_data["Temp (C)"]
         self.blood_flow = self.subject_data["Blood Flow (ml/s)"]
         print('Subject ' + self.subject_id + ' initialized')
-
-
-
         
 
 ### Aufgabe 2: Datenverarbeitung ###
